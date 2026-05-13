436c6f8f5663f0038c2198d0734fda899529ac27
Daniel Moret Robledo <danielmoretrobledo@gmail.com>
2026-05-13 21:56:46 +0000

    feat: añadir email y fecha de creación a ususuario

--- femverd-api/app/models/user.py
+++ femverd-api/app/models/user.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy import Column, Integer, String, Float, DateTime
+from datetime import datetime
 from ..database import Base 
 
 class User(Base):
@@ -6,6 +7,8 @@ class User(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     encrypted_dni = Column(String, unique=True, index=True)
+    email = Column(String, unique=True, index=True)  
     user_name = Column(String)
     points_balance = Column(Float, default=0.0)
     hashed_password = Column(String)
+    created_at = Column(DateTime, default=datetime.utcnow) 
