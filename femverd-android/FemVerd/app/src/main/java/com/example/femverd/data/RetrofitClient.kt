d6e789a9e797003962d308eacc85d4c22a9e7e2e
Daniel Moret <danielmoretrobledo@gmail.com>
2026-05-14 19:10:59 +0200

    feat: añadir modelos, gestor de token JWT y endpoints para comunicación con la API

--- femverd-android/FemVerd/app/src/main/java/com/example/femverd/data/RetrofitClient.kt
+++ femverd-android/FemVerd/app/src/main/java/com/example/femverd/data/RetrofitClient.kt
@@ -1,2 +1,17 @@
 package com.example.femverd.data
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+    //URL de la API (AWS - http://13.48.42.5:8000/)
+    private const val BASE_URL = "http://13.48.42.5:8000/"
+
+    val instance: ApiService by lazy {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        retrofit.create(ApiService::class.java)
+    }
+}
--- femverd-android/FemVerd/app/src/main/java/com/example/femverd/model/Models.kt
+++ femverd-android/FemVerd/app/src/main/java/com/example/femverd/model/Models.kt
@@ -1,2 +1,12 @@
 package com.example.femverd.model
 
+data class LoginResponse(val access_token: String, val token_type: String)
+data class UserMe(val dni: String, val name: String, val current_points: Double)
+data class HistoryItem(
+    val id: Int,
+    val date: String,
+    val quantity: Double,
+    val generated_points: Double,
+    val material_id: Int,
+    val green_point_id: Int
+)
