cde24246e944cf5f73a51f57ea78038a213d33a2
Daniel Moret <danielmoretrobledo@gmail.com>
2026-05-16 13:56:44 +0200

    feat: añadir prueba unitaria para comprobar el calculo de nivel del usuario

---
+++ femverd-android/FemVerd/app/src/androidTest/java/com/example/femverd/LoginUiTest.kt
@@ -0,0 +1,4 @@
+package com.example.femverd
+
+class LoginUiTest {
+}
---
+++ femverd-android/FemVerd/app/src/test/java/com/example/femverd/BusinessLogicTest.kt
@@ -0,0 +1,21 @@
+package com.example.femverd
+
+import org.junit.Test
+import org.junit.Assert.assertEquals
+
+class BusinessLogicTest {
+// Unit test
+    @Test
+    fun calculateUserLevel_isCorrect() {
+        // Arrange
+        val currentPoints = 750.0
+        val expectedLevel = 3 // 750 points means Level 2, because is 500 pts per level
+
+        // Act
+        // The business logic used in DashboardContent
+        val calculatedLevel = (currentPoints / 500).toInt() + 1
+
+        // Assert
+        assertEquals("The calculated level should exactly match the expected level", expectedLevel, calculatedLevel)
+    }
+}
